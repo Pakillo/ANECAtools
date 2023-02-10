fb5840c56f02f4a6d0ccffe5abee07e96c129c59
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2023-02-10 14:01:00 +0100

    preguntar si procesar cada publicacion o saltarla

--- R/publicaciones.R
+++ R/publicaciones.R
@@ -15,7 +15,7 @@
 #' bibfile <- system.file("extdata", "articulo.bib", package = "ANECAtools")
 #' introducir_publicaciones(bibfile)
 #' }
-introducir_publicaciones <- function(bibfile = NULL, mayus = TRUE, pausa = 3) {
+introducir_publicaciones <- function(bibfile = NULL, mayus = TRUE, pausa = 4) {
 
   stopifnot(is.character(bibfile))
   bib <- suppressWarnings(bib2df::bib2df(bibfile))
@@ -28,75 +28,81 @@ introducir_publicaciones <- function(bibfile = NULL, mayus = TRUE, pausa = 3) {
 
 
 
-procesar_publicacion <- function(bib = NULL, mayus = TRUE, pausa = 3) {
+procesar_publicacion <- function(bib = NULL, mayus = TRUE, pausa = 4) {
 
-  message("Procesando '", bib$TITLE, "'...\n")
+  procesar <- utils::askYesNo(paste0("Quieres procesar '", bib$TITLE, "'?"))
 
-  ## Autores
-  auts <- unlist(bib$AUTHOR)
-  if (isTRUE(mayus)) auts <- toupper(auts)
-  message("Se han identificado ", length(auts), " autores:\n\n", paste(auts, collapse = "\n"))
+  if (isTRUE(procesar)) {
 
-  pegar.auts <- utils::askYesNo("Quieres ir copiando los autores al portapapeles?")
-  if (isTRUE(pegar.auts)) {
-    pegar_autores(auts, pausa = pausa)
-  }
+    ## Autores
+    auts <- unlist(bib$AUTHOR)
+    if (isTRUE(mayus)) auts <- toupper(auts)
+    message("Se han identificado ", length(auts), " autores:\n\n", paste(auts, collapse = "\n"))
 
-  ## Titulo
-  utils::askYesNo("Copiando Titulo al portapapeles")
-  pegar_texto(bib$TITLE, mayus = mayus)
+    pegar.auts <- utils::askYesNo("Quieres ir copiando los autores al portapapeles?")
+    if (isTRUE(pegar.auts)) {
+      pegar_autores(auts, pausa = pausa)
+    }
+
+    ## Titulo
+    utils::askYesNo("Copiando Titulo al portapapeles")
+    pegar_texto(bib$TITLE, mayus = mayus)
 
 
-  ## Revista/Libro
-  if (bib$CATEGORY == "INCOLLECTION") {
-    utils::askYesNo("Copiando Titulo del Libro al portapapeles")
-    pegar_texto(bib$BOOKTITLE, mayus = mayus)
+    ## Revista/Libro
+    if (bib$CATEGORY == "INCOLLECTION") {
+      utils::askYesNo("Copiando Titulo del Libro al portapapeles")
+      pegar_texto(bib$BOOKTITLE, mayus = mayus)
+    } else {
+      utils::askYesNo("Copiando Revista al portapapeles")
+      pegar_texto(bib$JOURNAL, mayus = mayus)
+    }
+
+
+
+    ## Volumen
+    utils::askYesNo("Copiando Volumen al portapapeles")
+    pegar_texto(bib$VOLUME)
+
+
+    ## Paginas
+    pages <- unlist(strsplit(bib$PAGES, "--"))
+
+    utils::askYesNo("Copiando Pagina de inicio al portapapeles")
+    pegar_texto(pages[1])
+
+    if (length(pages) > 1) {
+      utils::askYesNo("Copiando Pagina de fin al portapapeles")
+      pegar_texto(pages[2])
+    }
+
+
+
+    ## Editorial
+    if (!is.na(bib$PUBLISHER)) {
+      utils::askYesNo("Copiando Editorial al portapapeles")
+      pegar_texto(bib$PUBLISHER)
+    }
+
+
+    ## Year
+    utils::askYesNo("Copiando Anyo al portapapeles")
+    pegar_texto(bib$YEAR)
+
+    ## ISSN/ISBN
+    if (bib$CATEGORY == "INCOLLECTION" | bib$CATEGORY == "BOOK" ) {
+      utils::askYesNo("Copiando ISBN al portapapeles")
+      pegar_texto(bib$ISBN)
+    } else {
+      utils::askYesNo("Copiando ISSN al portapapeles")
+      pegar_texto(bib$ISSN)
+    }
+
+
+    message("Listo! ----------------------------------------------------")
+
   } else {
-    utils::askYesNo("Copiando Revista al portapapeles")
-    pegar_texto(bib$JOURNAL, mayus = mayus)
+    message("Ok!")
   }
-
-
-
-  ## Volumen
-  utils::askYesNo("Copiando Volumen al portapapeles")
-  pegar_texto(bib$VOLUME)
-
-
-  ## Paginas
-  pages <- unlist(strsplit(bib$PAGES, "--"))
-
-  utils::askYesNo("Copiando Pagina de inicio al portapapeles")
-  pegar_texto(pages[1])
-
-  if (length(pages) > 1) {
-    utils::askYesNo("Copiando Pagina de fin al portapapeles")
-    pegar_texto(pages[2])
-  }
-
-
-
-  ## Editorial
-  if (!is.na(bib$PUBLISHER)) {
-    utils::askYesNo("Copiando Editorial al portapapeles")
-    pegar_texto(bib$PUBLISHER)
-  }
-
-
-  ## Year
-  utils::askYesNo("Copiando Anyo al portapapeles")
-  pegar_texto(bib$YEAR)
-
-  ## ISSN/ISBN
-  if (bib$CATEGORY == "INCOLLECTION" | bib$CATEGORY == "BOOK" ) {
-    utils::askYesNo("Copiando ISBN al portapapeles")
-    pegar_texto(bib$ISBN)
-  } else {
-    utils::askYesNo("Copiando ISSN al portapapeles")
-    pegar_texto(bib$ISSN)
-  }
-
-
-  message("Listo! ----------------------------------------------------")
 
 }
